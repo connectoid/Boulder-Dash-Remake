c5f974cde330b893c429f110c20349b14a03606c
Alexander Beley <beleysasha@gmail.com>
2025-11-28 15:43:06 +1200

    stones faalling and rolling complited

--- elements/stone/stone_static_body_2d.gd
+++ elements/stone/stone_static_body_2d.gd
@@ -16,7 +16,9 @@ func update_window_size():
 
 func _process(delta):
 	var vertical_dir = Vector2.ZERO
+	var horizontal_dir = Vector2.ZERO
 	vertical_dir.y = 1
+	horizontal_dir.x = -1
 	var final_direction = vertical_dir
 	var new_position = position + vertical_dir.normalized() * TILE_SIZE
 	#if vertical_dir.length_squared() > 0 && is_valid_position(new_position):
@@ -24,7 +26,15 @@ func _process(delta):
 		vertical_dir.y += 1
 		new_position = position + vertical_dir.normalized() * TILE_SIZE
 		position = new_position
-		#try_move(final_direction)
+	if not is_no_colliders(position) && is_no_walls(position):
+		if can_rolldown(position) == 'left':
+			new_position = position + horizontal_dir.normalized() * TILE_SIZE
+			position = new_position
+		elif can_rolldown(position) == 'right': 
+			new_position = position - horizontal_dir.normalized() * TILE_SIZE
+			position = new_position
+
+	#try_move(final_direction)
 
 
 func get_objects_in_pos(pos):
@@ -38,13 +48,41 @@ func get_objects_in_pos(pos):
 	return results
 
 
+func comape_lists(source_list, dest_list):
+	# Проверяет есть ли вхождение любого элемента списка source_list
+	# в список dest_list. Если есть, то возвращает true, иначе false
+	for item in source_list:
+		if dest_list.has(item):
+			return true
+	return false
+
+
+
+func can_rolldown(pos):
+	print('Start can falldown')
+	var denied_colliders_list = ['StaticBody2D', 'TileMapLayer', 'CharacterBody2D']
+	var pos_below_aside_left = Vector2(pos.x - TILE_SIZE.x, pos.y + TILE_SIZE.y)
+	var pos_below_aside_right = Vector2(pos.x + TILE_SIZE.x, pos.y + TILE_SIZE.y)
+	var pos_below = Vector2(pos.x, pos.y + TILE_SIZE.y)
+	var collisions_below_aside_left = get_objects_in_pos(pos_below_aside_left)
+	var collisions_below_aside_right = get_objects_in_pos(pos_below_aside_right)
+	var collisions_below = get_objects_in_pos(pos_below)
+	var is_empty_below_aside_left = not comape_lists(collisions_below_aside_left, denied_colliders_list)
+	var is_empty_below_aside_right = not comape_lists(collisions_below_aside_right, denied_colliders_list)
+	var is_stone_below = comape_lists(collisions_below, ['StaticBody2D'])
+	#print('Is stone below: ', is_stone_below)
+	#print('Is empty below aside: ', is_empty_below_aside)
+	if is_empty_below_aside_left && is_stone_below:
+		return 'left'
+	elif is_empty_below_aside_right && is_stone_below:
+		return 'right'
+	return 'no'
+
 func is_no_colliders(pos):
-	var colliders_list = ['StaticBody2D', 'TileMapLayer', 'CharacterBody2D']
-	var denied_colliders_list = get_objects_in_pos(pos)
-	for collider in colliders_list:
-		if denied_colliders_list.has(collider):
-			return false
-	return true
+	var denied_colliders_list = ['StaticBody2D', 'TileMapLayer', 'CharacterBody2D']
+	var colliders_list = get_objects_in_pos(pos)
+	return not comape_lists(colliders_list, denied_colliders_list)
+
 
 func is_no_walls(pos):
 	var is_valid = pos.x >= 0 && pos.x <= window_size.x && pos.y >= 0 && pos.y <= window_size.y
